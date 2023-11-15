8f8b425535df7b1e59193742c83210bb9b60addb
Yuyutsu-417 <ybamal977@gamil.com>
2023-11-15 11:22:13 +0530

    counter app

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+let add = document.querySelector(".add-btn");
+let save = document.querySelector(".save-btn");
+let value = document.querySelector("h2");
+let savedValue = document.querySelector("p");
+let reset = document.querySelector(".reset")
+
+
+add.addEventListener("click", () => {
+    value.innerHTML = +value.innerHTML + 1;
+})
+
+save.addEventListener("click", () => {
+    if (value.innerHTML == 0) { 
+        alert("Please add some value :)")
+    }
+    else {
+        if (savedValue.innerHTML == 0) {
+            savedValue.innerHTML = value.innerHTML
+        }
+        else {
+            savedValue.innerHTML = savedValue.innerHTML + ',' + value.innerHTML
+        }
+    }
+    value.innerHTML = 0
+})
+
+reset.addEventListener("click", () => {
+    value.innerHTML = 0;
+    savedValue.innerHTML = 0
+})
